0929fcc63d54f672702d9ce93e2ac9a9c9446d23
antonie <53366054+antonie-riziki@users.noreply.github.com>
2025-07-23 15:49:17 +0000

    added dashboard games and sidebar html templates

--- playora/playora_app/urls.py
+++ playora/playora_app/urls.py
@@ -8,5 +8,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
-
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('games', views.games_dashboard, name='games')
 ]
--- playora/playora_app/views.py
+++ playora/playora_app/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+
+def games_dashboard(request):
+    return render(request, 'games.html')
